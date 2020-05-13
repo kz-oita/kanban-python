25c0d41d4f395f903244bb275de4783de87a98e1
kz-oita <taoi.kizuka@gmail.com>
2020-05-13 11:39:23 +0900

    add function create card

--- kanban/views.py
+++ kanban/views.py
@@ -5,10 +5,11 @@ from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, resolve_url
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, UpdateView, CreateView, ListView, DeleteView
-from .forms import UserForm, ListForm
-from . models import List
-from django.contrib.auth.decorators import login_required
+from .forms import UserForm, ListForm, CardForm
 from .mixins import OnlyYouMixin
+from . models import List, Card
+from django.contrib.auth.decorators import login_required
+
 
 
 def index(request):
@@ -76,3 +77,13 @@ class ListDeleteView(LoginRequiredMixin, DeleteView):
     template_name = "kanban/lists/delete.html"
     form_class = ListForm
     success_url = reverse_lazy("kanban:lists_list")
+
+class CardCreateView(LoginRequiredMixin, CreateView):
+    model = Card
+    template_name = "kanban/cards/create.html"
+    form_class = CardForm
+    success_url = reverse_lazy("kanban:home")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
